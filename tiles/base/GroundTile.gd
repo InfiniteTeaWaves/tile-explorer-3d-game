45898001658f00e2fb4c3b883aeedfb7b5f7d5f3
Infinite Tea Wave <infiniteteawaves@gmail.com>
2024-01-26 22:22:26 +0100

    working editor script for the tile creation BaseTile in the editor script restructering

--- tiles/base/GroundTile.gd
+++ tiles/base/GroundTile.gd
@@ -1,6 +1,7 @@
 class_name GroundTile extends Node3D #Extend BasicTile and use biome_data
 
 var biome_data: BiomeData
+var top_material: StandardMaterial3D
 
 func _ready():
 	pass
@@ -10,11 +11,13 @@ func _process(delta):
 
 #func set_biome_data(data: BiomeData):
 	#biome_data = data
-	
+
+#sollte von außen erzeugt werden? nicht hier in script
 func create_ground_tile(i_biome_data):
 	biome_data = i_biome_data
 	self._set_top_layer_color()
 	
+#sollte von außen erzeugt werden? nicht hier in script
 func _set_top_layer_color():
 	var TopLayer = $TopLayer
 	var material_top = biome_data.ground_material_top
---
+++ tiles/base/baseTile.gd
@@ -0,0 +1,11 @@
+extends Node3D
+class_name BaseTile
+
+@export var tile_properties: TileProperties
+@export var biome_data: BiomeData
+
+func _ready():
+	pass 
+
+func _process(delta):
+	pass
